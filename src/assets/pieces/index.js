63e0935d1c11fe7ed2173939fe3cbca554e43c24
ctborg <coryborg@gmail.com>
2025-08-29 12:40:38 -0700

    Create assets folder for 3d pieces

---
+++ src/assets/pieces/index.js
@@ -0,0 +1,16 @@
+import king from "./king.glb";
+import queen from "./queen.glb";
+import rook from "./rook.glb";
+import bishop from "./bishop.glb";
+import knight from "./knight.glb";
+import pawn from "./pawn.glb";
+
+// Map chess.js piece types -> model files
+export default {
+  k: king,
+  q: queen,
+  r: rook,
+  b: bishop,
+  n: knight,
+  p: pawn
+};

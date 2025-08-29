25815de84383b861d4562bb321b8289283d44d41
ctborg <coryborg@gmail.com>
2025-08-29 15:30:12 -0700

    Create 2d index.js

---
+++ src/assets/pieces/pieces2d/index.js
@@ -0,0 +1,14 @@
+// src/assets/pieces2d/index.js
+import wP from "./merida/wP.svg"; import bP from "./merida/bP.svg";
+import wN from "./merida/wN.svg"; import bN from "./merida/bN.svg";
+import wB from "./merida/wB.svg"; import bB from "./merida/bB.svg";
+import wR from "./merida/wR.svg"; import bR from "./merida/bR.svg";
+import wQ from "./merida/wQ.svg"; import bQ from "./merida/bQ.svg";
+import wK from "./merida/wK.svg"; import bK from "./merida/bK.svg";
+
+const merida = {
+  P: { w: wP, b: bP }, N: { w: wN, b: bN }, B: { w: wB, b: bB },
+  R: { w: wR, b: bR }, Q: { w: wQ, b: bQ }, K: { w: wK, b: bK }
+};
+
+export default { merida };
